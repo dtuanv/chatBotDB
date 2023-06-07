8d65070a3b13c6795fdc0792b6410cb5aaa009aa
dtuanv <dtuanlieb@gmail.com>
2023-06-07 18:20:00 +0200

    first commit

---
+++ src/main/java/com/example/chatbotdb/ChatBotDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatbotdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatbotdb/ChatBotDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatbotdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
